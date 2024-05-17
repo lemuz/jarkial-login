886de0e5832de116833533ef15efe488fa27ff72
zkato <lemuzz101@gmail.com>
2024-05-17 16:32:34 -0600

    cambiando diseno

--- login-web-scss/src/app/shared/services/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-import { UtilMethods } from "../utils/utilMethods";
-
-@Injectable({
-    providedIn: 'root'
-})
-export class AuthService extends UtilMethods{
-    
-}
--- login-web-scss/src/app/shared/utils/Constants.ts
+++
@@ -1,7 +0,0 @@
-export abstract class Constants{
-
-    CODE_HTTP_SUCCESS: string = '0000';
-
-    KEY_ENCRYPT: string = 'JARKIAL_SV';
-    
-}
